89ed048a55daefb1eddb202303791094ab961c3a
brislazaro <brisa.lazaroc@gmail.com>
2024-07-17 13:25:16 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -7,7 +8,9 @@ function App() {
   return (
     <>
       <Header />
-      <Layout>hola</Layout>
+      <Layout>
+        <Outlet />
+      </Layout>
       <Footer />
     </>
   );
