e89034cb90d853323fb5abf5e5f312e0e10d16c0
brislazaro <brisa.lazaroc@gmail.com>
2024-07-17 12:35:21 +0200

    create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import Styles from "./footer.module.css";
+
+const Footer: FC = () => {
+  return (
+    <footer className={Styles.footer}>&copy; Created by Brisa Lazaro</footer>
+  );
+};
+
+export default Footer;
