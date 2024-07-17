50971b819977a0aa45e2804e6b9aa15081e8ab43
brislazaro <brisa.lazaroc@gmail.com>
2024-07-17 12:24:43 +0200

    create header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import style from "./header.module.css";
+import logo from "../../assets/img/logo.webp";
+
+const Header: FC = () => {
+  return (
+    <header className={style.header}>
+      <img src={logo} alt="logo" className={style.logo} />
+      <h1> VW technical test</h1>
+    </header>
+  );
+};
+
+export default Header;
