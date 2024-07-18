23a808db07a2342df1660d6efb3276e22149bd1d
brislazaro <brisa.lazaroc@gmail.com>
2024-07-18 10:37:04 +0200

    create table

--- src/pages/ToDosPage/ToDosPage.tsx
+++ src/pages/ToDosPage/ToDosPage.tsx
@@ -1,11 +1,31 @@
 import useToDos from "./useToDos";
+import { Table } from "antd";
 
 const ToDosPage = () => {
   const { isLoading, isError, data } = useToDos();
 
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Completed",
+      dataIndex: "completed",
+      key: "completed",
+      render: (completed: boolean) => {
+        return (
+          <>{completed ? <p>Completed</p> : <button>Complete task</button>}</>
+        );
+      },
+    },
+  ];
+
   return (
     <>
       <h2>To Do´s Page</h2>
+      <Table dataSource={data} columns={columns} />
     </>
   );
 };
