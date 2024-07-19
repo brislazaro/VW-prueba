d6dfd1972516b42c633ef77d16af83f0a45384cc
brislazaro <brisa.lazaroc@gmail.com>
2024-07-19 16:21:59 +0200

    add async redux actions to fetch todos

--- src/redux/slices/toDosSlice.ts
+++ src/redux/slices/toDosSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Todo } from "../../components/Types/Types";
 
 type ToDosState = {
@@ -6,6 +6,13 @@ type ToDosState = {
   isLoading: boolean;
   isError: boolean;
 };
+
+// Using any here due to an issue with types and redux-toolkit
+export const fetchToDos: any = createAsyncThunk("fetchToDos", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const apiData: Todo[] = await response.json();
+  return apiData;
+});
 
 const initialState: ToDosState = {
   toDos: [],
@@ -17,6 +24,23 @@ const toDosSlice = createSlice({
   name: "toDos",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchToDos.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchToDos.rejected, (state) => {
+      state.isLoading = false;
+      state.isError = true;
+    });
+    builder.addCase(
+      fetchToDos.fulfilled,
+      (state, action: PayloadAction<Todo[]>) => {
+        state.isError = false;
+        state.isLoading = false;
+        state.toDos = action.payload;
+      }
+    );
+  },
 });
 
 export const actions = toDosSlice.actions;
