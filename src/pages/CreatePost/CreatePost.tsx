e50114e1ca975cb965bba02be86521c121e134d2
brislazaro <brisa.lazaroc@gmail.com>
2024-07-22 16:26:06 +0200

    Add basic navigation to create drawer and actions

--- src/pages/CreatePost/CreatePost.tsx
+++ src/pages/CreatePost/CreatePost.tsx
@@ -1,5 +1,49 @@
+import { Button, Drawer, Space } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+type CreateFormState = {
+  title: string;
+  body: string;
+  userId: string;
+};
+
+const initialState = {
+  title: "",
+  body: "",
+  userId: "",
+};
+
 const CreatePost = () => {
-  return <>it works</>;
+  const [open, setOpen] = useState(true);
+  const [formState, setFormState] = useState<CreateFormState>(initialState);
+
+  const navigate = useNavigate();
+
+  const onClose = () => {
+    setOpen(false);
+
+    setTimeout(() => {
+      navigate("/");
+    }, 250);
+  };
+  return (
+    <Drawer
+      open={open}
+      onClose={onClose}
+      size="large"
+      title="Add post"
+      placement="right"
+      extra={
+        <Space>
+          <Button>Cancel</Button>
+          <Button type="primary">Save</Button>
+        </Space>
+      }
+    >
+      it works
+    </Drawer>
+  );
 };
 
 export default CreatePost;
