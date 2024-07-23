6a28b6ec58c027ae999bbd03290867b21791427e
brislazaro <brisa.lazaroc@gmail.com>
2024-07-23 11:47:45 +0200

    Ignore e2e in vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -8,5 +8,6 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./tests/setup.ts",
     globals: true,
+    exclude: [...configDefaults.exclude, "e2e/*"],
   },
 });
